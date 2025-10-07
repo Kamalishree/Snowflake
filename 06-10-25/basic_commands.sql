ba89befc0e09bbae5291febf623d55d1a877017a
Kamalishree M <113345175+Kamalishree@users.noreply.github.com>
2025-10-07 11:17:22 -0700

    Add files via upload

---
+++ 06-10-25/basic_commands.sql
@@ -0,0 +1,7 @@
+select current_role();  ---- account admin
+select current_user();  --- haritha 
+show roles;
+drop role snowflake_learning_role;
+use role orgadmin;
+select current_role();
+use role accountadmin;
